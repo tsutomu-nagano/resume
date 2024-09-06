a4a6df90cbb76f13b39252c6939b9b318a8a7ffe
tsutomu-nagano <copyxshow@gmail.com>
2024-09-06 17:54:33 +0900

    集計事項の検索機能のコメント解除

--- src/L1/resume/app/components/Header.tsx
+++ src/L1/resume/app/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
             <div className="navbar bg-base-100">
                 <a className="btn btn-ghost text-xl">ReSUME L1</a>
                 <SearchItemSelector labelja="統計調査" labelen="" ref_names = {["TABLELISTs"]} resource_name="STATLIST" resource_field="STATNAME" kind="stat" />
-                {/* <SearchItemSelector labelja="集計事項" labelen="" ref_name = "TABLELISTs" resource_name="TABLE_MEASURE" resource_field="NAME" kind="measure" /> */}
+                <SearchItemSelector labelja="集計事項" labelen="" ref_names = {["TABLE_MEASUREs","TABLELIST"]} resource_name="MEASURELIST" resource_field="NAME" kind="measure" />
                 <SearchItemSelector labelja="分類事項" labelen="" ref_names = {["TABLE_DIMENSIONs", "TABLELIST"]} resource_name="DIMENSIONLIST" resource_field="CLASS_NAME" kind="dimension" />
             </div>
             <div className="ml-4"><SearchCountResult /></div>
