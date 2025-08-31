eebc0db8f2c531518bc1d960d6f6a4b578ac6864
tsutomu-nagano <copyxshow@gmail.com>
2025-08-31 11:14:32 +0900

    入力を必須じゃないように修正

--- src/L1/resume/app/schemas/yearSchema.ts
+++ src/L1/resume/app/schemas/yearSchema.ts
@@ -5,16 +5,16 @@ import * as z from 'zod';
 const numPattern = "[0-9０-９]";
 const eraPattern = "([SHRshrＳＨＲｓｒｈ]|昭和|平成|令和)"
 
-const yearRegexp  = new RegExp(`^(${numPattern}{4}|${eraPattern}(${numPattern}{1,2}|元))年?$`)
+const yearRegexp = new RegExp(`^(${numPattern}{4}|${eraPattern}(${numPattern}{1,2}|元))年?$`)
 // const yearRegexp  = /^[0-9０-９]{4}年?$/
 
 // 全角→半角
 function normalize(input: string): string {
   return input.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) =>
-            String.fromCharCode(s.charCodeAt(0) - 0xfee0)
-          )
-          // 末尾の「年」(全角/半角どちらでも) を削除
-        .replace(/年$/, "");
+    String.fromCharCode(s.charCodeAt(0) - 0xfee0)
+  )
+    // 末尾の「年」(全角/半角どちらでも) を削除
+    .replace(/年$/, "");
 }
 
 // 西暦に変換
@@ -44,11 +44,15 @@ function toAD(yearStr: string): number | null {
 
 export const yearSchema = z
   .string()
-  .regex(yearRegexp, { message: "西暦4桁または和暦で入力してください" })
+  .optional()
+  .refine(val => !val || yearRegexp.test(val), {
+    message: "西暦4桁または和暦で入力してください",
+  })
   .transform((val) => {
+    if (!val) return "";
     const ad = toAD(val);
     if (ad === null) {
       throw new Error("変換できません");
     }
-    return ad;
+    return String(ad);
   });
