37deb79c96fadc737c9df7549472b17a6e7e1803
tsutomu-nagano <copyxshow@gmail.com>
2025-08-31 11:15:32 +0900

    調査年の検索のテストコードや不要な部分を削除

--- src/L1/resume/app/components/TimeSelector.container.tsx
+++ src/L1/resume/app/components/TimeSelector.container.tsx
@@ -1,40 +1,22 @@
 // src/app/StatCard.tsx
 "use client";
 
-import React, { useState, useMemo } from 'react';
-import { createApolloClient } from '@/lib/apolloClient';
-import { GET_SEARCH_TAG_LIST } from '../../lib/queries';
-
+// import React, { useState, useMemo } from 'react';
 import { useSearchItem } from '../contexts/SearchItemsProvider';
-
 import { TimeSelector } from './TimeSelector';
 
 
 export function TimeSelectorContainer() {
 
-  const ref_names = ["TABLE_DIMENSIONs", "TABLELIST"]
-  const resource_name = "DIMENSIONLIST"
-  const resource_field = "CLASS_NAME"
+  const { addItem, removeItem } = useSearchItem();
 
-  const [searchTerm, setSearchTerm] = useState('');
-  const client = createApolloClient();
-
-  const [fromYear, setFromYear] = useState<number | null>(null);
-  const [toYear, setToYear] = useState<number | null>(null);
-
-
-  // const searchConditionClick = () => {
-  //   isSelected ? removeItem(kind, name) : addItem(kind, name);
-  //   setIsSelected(!isSelected);
-  // };
-
-  const test = () => {
-    alert(`${fromYear} - ${toYear}`);
+  const HandleSearch = (fromYear: String, toYear:String) => {
+    addItem("time", `${fromYear}-${toYear}`);
   };
 
 
   return (
-    <TimeSelector onChangeFrom={setFromYear} onChangeTo={setToYear} onEnter={test} />
+    <TimeSelector onSubmit={HandleSearch} />
   );
 }
 
--- src/L1/resume/app/components/TimeSelector.tsx
+++ src/L1/resume/app/components/TimeSelector.tsx
@@ -1,13 +1,12 @@
 // src/app/StatCard.tsx
 "use client";
 
-import { FaChevronDown, FaCircleExclamation } from "react-icons/fa6";
+import { FaChevronDown } from "react-icons/fa6";
 import { renderIconByKind } from "../common/convertor";
 import React from "react";
-import { on } from "events";
 
 import { yearSchema } from "@schemas/yearSchema";
-import { useForm, useWatch, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 
@@ -15,53 +14,42 @@ import * as z from 'zod';
 const schema = z.object({
   fromYear: yearSchema,
   toYear: yearSchema
-// }).refine(({ fromYear, toYear }) => Number(fromYear) <= Number(toYear), {
-//   message: "終了年は開始年以降を指定してください",
-//   path: ["toYear"],
+  // }).refine(({ fromYear, toYear }) => Number(fromYear) <= Number(toYear), {
+  //   message: "終了年は開始年以降を指定してください",
+  //   path: ["toYear"],
 });
 
 type Schema = z.infer<typeof schema>;
 
 
 interface TimeSelectorProps {
-  onEnter: () => void;
-  onChangeFrom: (value: number) => void; // ← 開始年を親に渡す
-  onChangeTo: (value: number) => void;   // ← 終了年を親に渡す
-  onSubmit: () => void;
+  onSubmit: (fromYear: String, toYear: String) => void;
 }
 
-export function TimeSelector({ onEnter, onChangeFrom, onChangeTo, onSubmit }: TimeSelectorProps) {
+export function TimeSelector({ onSubmit }: TimeSelectorProps) {
 
   const {
-    control,
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Schema>({
+    defaultValues: {
+      fromYear: undefined,
+      toYear: undefined
+    },
     resolver: zodResolver(schema),
-    mode: "all",
+    mode: "onBlur",
   });
-
-
-console.log(schema.safeParse({ fromYear: "2024", toYear : "2014" }).success); // true
-console.log(schema.safeParse({ fromYear: "2024年", toYear : "2014年" }).success); // true
-console.log(schema.safeParse({ fromYear: "Ｒ1年", toYear : "2014" }).success); // true
-console.log(schema.safeParse({ fromYear: "H12", toYear : "2014" }).success);  // true
-console.log(schema.safeParse({ fromYear: "元" , toYear : "2014"}).success);   // false
 
 
   const kind = "time";
   const labelja = "調査年"
 
-  console.log(errors.fromYear);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      console.log("Enter key pressed");
-      onEnter();
-    }
+  const handleFormSubmit: SubmitHandler<Schema> = (data) => {
+    // data.fromYear, data.toYear は既に数値化されている想定
+    onSubmit(data.fromYear, data.toYear);
   };
-
 
   return (
     <div className="dropdown">
@@ -73,30 +61,28 @@ console.log(schema.safeParse({ fromYear: "元" , toYear : "2014"}).success);   /
         className="dropdown-content card card-compact bg-base-100 z-[1] w-[400px] p-2 shadow">
         <div className="card-body">
 
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
             <fieldset className="fieldset">
               <div className="flex flex-row items-center gap-3">
-                  <div>
-                    <legend className="fieldset-legend">開始年</legend>
-                    <input
-                      type="text"
-                      className={`input input-bordered ${errors.fromYear ? 'border-red-500' : ''} w-40`}
-                      placeholder="開始年"
-                      {...register("fromYear")}
-                      onKeyDown={handleKeyDown}
-                    />
-                  </div>
-                  <span>〜</span>
-                  <div>
-                    <legend className="fieldset-legend">終了年</legend>
-                    <input
-                      type="text"
-                      className="input input-bordered w-40"
-                      placeholder="終了年"
-                      {...register("toYear")}
-                      onKeyDown={handleKeyDown}
-                    />
-                  </div>
+                <div>
+                  <legend className="fieldset-legend">開始年</legend>
+                  <input
+                    type="text"
+                    className={`input input-bordered ${errors.fromYear ? 'border-red-500' : ''} w-40`}
+                    placeholder="開始年"
+                    {...register("fromYear")}
+                  />
+                </div>
+                <span>〜</span>
+                <div>
+                  <legend className="fieldset-legend">終了年</legend>
+                  <input
+                    type="text"
+                    className="input input-bordered w-40"
+                    placeholder="終了年"
+                    {...register("toYear")}
+                  />
+                </div>
               </div>
               {errors.fromYear && <p className="text-red-500">{errors.fromYear.message}</p>}
               {errors.toYear && <p className="text-red-500">{errors.toYear.message}</p>}
