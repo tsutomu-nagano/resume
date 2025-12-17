b1121f5bcfe1ac62ccdd8c99f46a6431da2b02de
tsutomu-nagano <copyxshow@gmail.com>
2025-12-17 12:45:25 +0900

    検索条件のテキストとアイコンの配置を修正

--- src/L1/resume/app/components/SearchItemSelector.tsx
+++ src/L1/resume/app/components/SearchItemSelector.tsx
@@ -72,7 +72,7 @@ export default function SearchItemSelector({ labelja, labelen = "", ref_names, r
 
   return (
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="flex flex-row gap-2 btn btn-outline m-1">
+      <div tabIndex={0} role="button" className="flex flex-row flex-nowrap gap-2 whitespace-nowrap btn btn-outline m-1">
         {renderIconByKind(kind)}{labelja}から探す<FaChevronDown />
       </div>
       <div
