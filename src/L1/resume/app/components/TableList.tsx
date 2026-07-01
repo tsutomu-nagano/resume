efe9cb07c347d60797fb8bfe6f42d6f66db8d33e
tsutomu NAGANO <59475213+tsutomu-nagano@users.noreply.github.com>
2026-07-01 13:49:07 +0900

    検索結果の表示切替キャッシュを追加

--- src/L1/resume/app/components/TableList.tsx
+++ src/L1/resume/app/components/TableList.tsx
@@ -1,64 +1,59 @@
 "use client"; // このファイルはクライアントサイドでのみ実行される
 
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 import { useSearchItem } from "../contexts/SearchItemsProvider";
 import { TableCard } from "./TableCard";
-import { InfiniteScrollContainer } from './InfiniteScrollContainer'
-
-
+import { InfiniteScrollContainer } from "./InfiniteScrollContainer";
 
 export default function TableList() {
+  const { loading, error, fetchMore, searchResult, isLast } = useSearchItem();
 
-    const {loading, error, fetchMore, searchResult, isLast } = useSearchItem();
+  const didEffect = useRef(false);
+  useEffect(() => {
+    if (!didEffect.current && searchResult.length === 0 && !isLast) {
+      didEffect.current = true;
+      fetchMore();
+    }
+  }, [fetchMore, isLast, searchResult.length]);
 
-    const didEffect = useRef(false);
-    useEffect(() => {
-      if (!didEffect.current) {
-        didEffect.current = true;
-        fetchMore();
-      }
-    }, []);
+  if (loading)
+    return <span className="loading loading-spinner text-primary"></span>;
+  if (error) return <p>Error: {error.message}</p>;
 
-
-    if (loading) return <span className="loading loading-spinner text-primary"></span>
-    if (error) return <p>Error: {error.message}</p>;
-    
-
-    return (
-        <InfiniteScrollContainer fetchMore={fetchMore} isLast={isLast} >
-        <div className="flex flex-col gap-y-10">
-                {searchResult.map(
-                    (tbl: {
-                        statdispid: string;
-                        cycle: string;
-                        statcode: string;
-                        survey_date: string;
-                        title: string;
-                        year_s: string;
-                        year_e: string;
-                        table_tags: { tag_name: string }[];
-                        table_measures: { name: string }[];
-                        table_dimensions: { class_name: string }[];
-                        table_regions: { regiontype: string }[];
-                    }) => (
-                        <TableCard
-                            key={tbl.statdispid}
-                            statdispid={tbl.statdispid}
-                            title={tbl.title}
-                            year_s={tbl.year_s}
-                            year_e={tbl.year_e}
-                            cycle={tbl.cycle}
-                            statcode={tbl.statcode}
-                            survey_date={tbl.survey_date}
-                            tags={tbl.table_tags}
-                            measures={tbl.table_measures}
-                            dimensions={tbl.table_dimensions}
-                            regions={tbl.table_regions}
-                        />
-                    )
-                )}
-        </div>
-            </InfiniteScrollContainer>
-    );
-};
-
+  return (
+    <InfiniteScrollContainer fetchMore={fetchMore} isLast={isLast}>
+      <div className="flex flex-col gap-y-10">
+        {searchResult.map(
+          (tbl: {
+            statdispid: string;
+            cycle: string;
+            statcode: string;
+            survey_date: string;
+            title: string;
+            year_s: string;
+            year_e: string;
+            table_tags: { tag_name: string }[];
+            table_measures: { name: string }[];
+            table_dimensions: { class_name: string }[];
+            table_regions: { regiontype: string }[];
+          }) => (
+            <TableCard
+              key={tbl.statdispid}
+              statdispid={tbl.statdispid}
+              title={tbl.title}
+              year_s={tbl.year_s}
+              year_e={tbl.year_e}
+              cycle={tbl.cycle}
+              statcode={tbl.statcode}
+              survey_date={tbl.survey_date}
+              tags={tbl.table_tags}
+              measures={tbl.table_measures}
+              dimensions={tbl.table_dimensions}
+              regions={tbl.table_regions}
+            />
+          ),
+        )}
+      </div>
+    </InfiniteScrollContainer>
+  );
+}
